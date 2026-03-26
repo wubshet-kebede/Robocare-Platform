65f790977e533dc1645310e7442f5cb8efe798e4
Wube <wubshet2829@gmail.comgit config --global user.name Wubshet>
2026-03-26 11:19:19 +0300

    Domain-Driven Design added

--- server/internal/domain/model/patient.go
+++ server/internal/domain/model/patient.go
@@ -1,15 +1,35 @@
 package model
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
 
 
 type Patient struct {
-	ID            uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	HospitalID    uint     `gorm:"not null" json:"hospital_id"`
-	FullName      string   `gorm:"size:255;not null" json:"full_name"`
+	ID            uuid.UUID `json:"id"`
+	HospitalID    uint     `json:"hospital_id"`
+	FullName      string   `json:"full_name"`
 	CurrentRoomID uint     `json:"current_room_id"`
-	Room          Room     `gorm:"foreignKey:CurrentRoomID"`
+	Room          Room     `json:"CurrentRoomID"`
 	MedicalStatus string   `json:"medical_status"` // "stable", "critical"
+}
+//  Domain-Driven Design (DDD)
+// new patient constructor with the validation logic for the required fields 
+func NewPatient(HospitalID uint, FullName string, CurrentRoomID uint, MedicalStatus string) (*Patient, error) {
+	if FullName == "" {
+		return nil, errors.New("full name is required")
+	}
+	if MedicalStatus == "" {
+		return nil, errors.New("medical status is required")
+	}
+
+	return &Patient{
+		ID:            uuid.New(),
+		HospitalID:    HospitalID,
+		FullName:      FullName,
+		CurrentRoomID: CurrentRoomID,
+		MedicalStatus: MedicalStatus,
+	}, nil
 }
--- server/internal/domain/model/user.go
+++ server/internal/domain/model/user.go
@@ -1,14 +1,53 @@
 package model
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
+
+// type User struct {
+// 	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+// 	FullName   string   `gorm:"size:255;not null" json:"full_name"`
+// 	Email      string   `gorm:"uniqueIndex;not null" json:"email"`
+// 	Password   string   `json:"-"` // Hashed password, never exported to JSON
+// 	Role       string   `gorm:"size:50;not null" json:"role"` // "doctor", "staff", "admin"
+// 	HospitalID uuid.UUID `gorm:"type:uuid;not null" json:"hospital_id"`
+// 	// Hospital   Hospital `gorm:"foreignKey:HospitalID" json:"hospital"`
+// }
+
 type User struct {
-	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	FullName   string   `gorm:"size:255;not null" json:"full_name"`
-	Email      string   `gorm:"uniqueIndex;not null" json:"email"`
-	Password   string   `json:"-"` // Hashed password, never exported to JSON
-	Role       string   `gorm:"size:50;not null" json:"role"` // "doctor", "staff", "admin"
-	HospitalID uuid.UUID `gorm:"type:uuid;not null" json:"hospital_id"`
-	// Hospital   Hospital `gorm:"foreignKey:HospitalID" json:"hospital"`
+	ID         uuid.UUID     `json:"id"`
+	FullName   string   `json:"full_name"`
+	Email      string   `json:"email"`
+	Password   string   `json:"-"`    // "-" means never send the password to the Frontend (Nuxt)
+	Role       string   `json:"role"` // "doctor" or "staff"
+	HospitalID uuid.UUID     `json:"hospital_id"`
+	// Hospital   Hospital `json:"hospital"` // Nested struct to show hospital details
+}
+
+func NewUser(FullName, Email, Password, Role  string, HospitalID uuid.UUID) (*User, error) {
+	if FullName == "" {
+		return nil, errors.New("full name is required")
+	}
+	if Email == "" {
+		return nil, errors.New("email is required")
+	}
+	if Password == "" {
+		return nil, errors.New("password is required")
+	}
+	if Role == "" {
+		return nil, errors.New("role is required")
+	}
+	if HospitalID == uuid.Nil {
+		return nil, errors.New("hospital ID is required")
+	}
+	return &User{
+		ID:         uuid.New(),
+		FullName:   FullName,
+		Email:      Email,
+		Password:   Password,
+		Role:       Role,
+		HospitalID: HospitalID,
+	}, nil
 }
