b24eb3b106ffbd557b036be5f81f1433cafb0db8
Wube <wubshet2829@gmail.comgit config --global user.name Wubshet>
2026-04-07 09:12:22 +0300

    registering the robot api completed

--- server-2/internal/api/robot/robot_handler.go
+++ server-2/internal/api/robot/robot_handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/wubshet-kebede/robocare-platform/server-2/internal/middleware"
 	"github.com/wubshet-kebede/robocare-platform/server-2/internal/services/robot"
 )
 
@@ -14,25 +15,27 @@ type RobotReq struct {
 }
 
 func CreateRobotHandler(w http.ResponseWriter, r *http.Request) {
-    // 1. Get HospitalID (Usually from your Auth Middleware/Context)
-    // Assuming you stored it as a string or UUID in context
-    hID := r.Context().Value("hospital_id").(uuid.UUID)
+     hospitalID , ok := r.Context().Value(middleware.HospitalIDKey).(uuid.UUID)
+	 if !ok  {
+		http.Error(w, "missing hosppital id", http.StatusBadRequest)
+		return
+	}
 
-    // 2. Decode the JSON body from Nuxt 3
+    
     var body RobotReq
     if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
         http.Error(w, "Invalid request body", http.StatusBadRequest)
         return
     }
-
-    // 3. Call the Service
-    newRobot, err := robot.RegisterRobot(hID, body.SerialNumber, body.Model)
+    if body.SerialNumber == "" {
+    http.Error(w, "serial_number is required", http.StatusBadRequest)
+    return
+}
+    newRobot, err := robot.RegisterRobot( hospitalID , body.SerialNumber, body.Model)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-
-    // 4. Return the new Robot as JSON
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusCreated)
     json.NewEncoder(w).Encode(newRobot)
