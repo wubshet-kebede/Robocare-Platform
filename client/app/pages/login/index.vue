d49e9898b30eab573c785a43111f82bd15588e42
Wubshet <wubshet2829@gmail.com>
2026-09-15 13:57:11 +0300

    fix vue toastification production SSR error

--- client/app/pages/login/index.vue
+++ client/app/pages/login/index.vue
@@ -1,9 +1,7 @@
 <script setup>
 import UiBaseInput from "~/components/ui/BaseInput.vue";
 import { useForm } from "vee-validate";
-import { useToast } from "vue-toastification";
-
-const toast = useToast();
+const { $toast } = useNuxtApp();
 definePageMeta({
   layout: false,
 });
@@ -25,11 +23,11 @@ const submit = handleSubmit(async (values) => {
     const userData = await me();
     user.value = userData;
     console.log("Logged user:", userData);
-    toast.success("Welcome back! Login successful.");
+    $toast.success("Welcome back! Login successful.");
     await navigateTo(`/${userData.hospital.slug}/dashboard`);
   } catch (err) {
     console.log(err);
-    toast.error("Login failed. Please check your email and password.");
+    $toast.error("Login failed. Please check your email and password.");
   } finally {
     loading.value = false;
   }
